6d4f02fd81c6c1f311d110e2ac8240f5f4413aaf
n48331 <unfolks@gmail.com>
2023-04-01 16:30:22 +0530

    images added

--- js/data.js
+++ js/data.js
@@ -1,36 +1,197 @@
-const data = [
-  {
-    name: "Nabeel",
-    cat:"logo"
-  },
-  {
-    name: "Razeena",
-    cat:"logo"
-  },
-  {
-    name: "Anjun",
-    cat:"footer"
-  },
-  {
-    name: "Nawaf",
-    cat:"logo"
-  },
-  {
-    name: "Shaheef",
-    cat:"icon"
-  },
-  {
-    name: "Saurav",
-    cat:"icon"
-  },
-  
-];
-
+$(document).ready(function () {
+  $("ul#searchFilter li").click(function () {
+    $(this).addClass("active").siblings("li").removeClass("active");
+    // console.log($('ul li').hasClass('active'));
+  });
+});
+const imagePath = "./images/";
 var persons = [];
 
-for (let i = 0; i < data.length; i++) {
+const data = [
+  {
+    name: "Janssen",
+    image: "footer_logo.png",
+    cat: "logo",
+    keywords: "",
+  },
+  {
+    name: "Janssen Medical Cloud",
+    image: "footer_logo_2.png",
+    cat: "logo",
+    keywords: "",
+  },
+  {
+    name: "Janssen With Me",
+    image: "JWM_bg.png",
+    cat: "logo",
+    keywords: "",
+  },
+  {
+    name: "Janssen With Me Blue",
+    image: "JWM_logo.png",
+    cat: "logo",
+    keywords: "",
+  },
+  {
+    name: "Janssen With Me White",
+    image: "Janssen_with_Me.png",
+    cat: "logo",
+    keywords: "",
+  },
+  {
+    name: "janssen oncology",
+    image: "janssen-oncology.png",
+    cat: "logo",
+    keywords: "",
+  },
+  {
+    name: "janssen oncology White",
+    image: "Oncology_logo.png",
+    cat: "logo",
+    keywords: "",
+  },
+  {
+    name: "Janssen Immunology",
+    image: "Janssen-Immunology_Logo.png",
+    cat: "logo",
+    keywords: "",
+  },
+  {
+    name: "Pharmacyclics",
+    image: "Logo_Pharmacyclics.png",
+    cat: "logo",
+    keywords: "",
+  },
+ 
+  
+  {
+    name: "Tremfya",
+    image: "CZ-EE-Tremfya Logo.png",
+    cat: "logo",
+    keywords: "czech replublic estonia",
+  },
+  {
+    name: "Tremfya",
+    image: "LT -Tremfya_Logo-.png",
+    cat: "logo",
+    keywords: "Luthvania",
+  },
+  {
+    name: "Erleada",
+    image: "CZ-Erleada_logo.png",
+    cat: "logo",
+    keywords: "slovenia",
+  },
+  {
+    name: "Erleada",
+    image: "HU-Erleada_logo.png",
+    cat: "logo",
+    keywords: "hungary",
+  },
+  {
+    name: "Erleada White",
+    image: "HU-Erleada_logo_white.png",
+    cat: "logo",
+    keywords: "hungary",
+  },
+  {
+    name: "Darzalex SC Hungary",
+    image: "Darzalex _SC_Logo.png",
+    cat: "logo",
+    keywords: "hungary",
+  },
+  {
+    name: "Darzalex SC Slovenia",
+    image: "SI_Darzalex _SC_Logo.png",
+    cat: "logo",
+    keywords: "slovenia",
+  },
+  {
+    name: "Arrow",
+    image: "arrow.png",
+    cat: "icon",
+    keywords: "bulgaria",
+  },
+  {
+    name: "Profile",
+    image: "Icon-1-01.png",
+    cat: "icon",
+    keywords: "czech republic",
+  },
+  {
+    name: "Target",
+    image: "Icon-2-02.png",
+    cat: "icon",
+    keywords: "slovakia",
+  },
+  {
+    name: "Do not forward",
+    image: "CZ_donotforward.png",
+    cat: "footer",
+    keywords: "czech republic",
+  },
+  {
+    name: "Border line",
+    image: "border_line2.png",
+    cat: "icon",
+    keywords: "czech",
+  },
+  {
+    name: "info",
+    image: "Image_2.png",
+    cat: "icon",
+    keywords: "czech republic",
+  },
+  {
+    name: "Do not forward",
+    image: "EE_donotforward.png",
+    cat: "footer",
+    keywords: "Estonia",
+  },
+  {
+    name: "Do not forward",
+    image: "HU_donotforward.png",
+    cat: "footer",
+    keywords: "hungary",
+  },
+  {
+    name: "Do not forward",
+    image: "SI_donotforward.png",
+    cat: "footer",
+    keywords: "slovenia",
+  },
+  {
+    name: "Border line",
+    image: "border_line.png",
+    cat: "icon",
+    keywords: "",
+  },
+  {
+    name: "Top shadow",
+    image: "top.png",
+    cat: "icon",
+    keywords: "",
+  },
+  {
+    name: "Bottom shadow",
+    image: "bottom.png",
+    cat: "icon",
+    keywords: "",
+  },
+ 
+  {
+    name: "Contact",
+    image: "Icon-3-03.png",
+    cat: "icon",
+    keywords: "",
+  },
+];
+
+for (var i = 0; i < data.length; i++) {
   persons.push({
     name: data[i].name,
+    image: imagePath+data[i].cat+'/' + data[i].image,
     cat: data[i].cat,
+    keywords: data[i].keywords,
   });
 }
--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 const resourcesList = document.getElementById("resourcesList");
 const searchBar = document.getElementById("searchQueryInput");
-const filter = document.getElementById("searchFilter");
 let hpResources = [];
+const filterItems = document.querySelectorAll("#searchFilter li");
+let searchTerm = "all";
+let searchString = ''
 
-$(document).ready(function () {
-  $("ul#searchFilter li").click(function () {
-    $(this).addClass("active").siblings("li").removeClass("active");
-    filterString = $(this).attr("cat");
+filterItems.forEach((filter) => {
+  filter.addEventListener("click", function () {
+    searchTerm = filter.innerHTML.toLowerCase();
+    const filteredResources = hpResources.filter((resource) => {
+      return (resource.name.toLowerCase().includes(searchString)||resource.keywords.toLowerCase().includes(searchString)) && (resource.cat.toLowerCase().includes(searchTerm) || searchTerm=='all');
+    });
+    displayResources(filteredResources);
   });
 });
 
 searchBar.addEventListener("keyup", (e) => {
-  const searchString = e.target.value.toLowerCase();
-
+  searchString = e.target.value.toLowerCase();
   const filteredResources = hpResources.filter((resource) => {
-    return resource.name.toLowerCase().includes(searchString);
+    return (resource.name.toLowerCase().includes(searchString)||resource.keywords.toLowerCase().includes(searchString)) && (resource.cat.toLowerCase().includes(searchTerm) || searchTerm=='all');
   });
   displayResources(filteredResources);
 });
@@ -22,9 +26,7 @@ searchBar.addEventListener("keyup", (e) => {
 const loadResources = async () => {
   try {
     const res = persons;
-    console.log(persons);
     hpResources = res;
-    console.error(hpResources);
     displayResources(hpResources);
   } catch (err) {
     console.error(err);
@@ -32,18 +34,23 @@ const loadResources = async () => {
 };
 
 const displayResources = (resources) => {
-  const htmlString = resources
-    .map((resource) => {
-      return `
-      <div class="card">
-      <div class="image">
-        <img src="./images/logos/Darzalex _SC_Logo.png" alt="">
+  let htmlString = 'No results'
+  if(resources.length!=0){
+    htmlString = resources
+      .map((resource) => {
+        return `
+        <div class="card">
+        <a href="${resource.image}" class="download" download><img src="./images/download-icon.png" alt="" >
+        </a>
+        <div class="image">
+          <img src="${resource.image}" alt="">
+        </div>
+        <p>${resource.name}</p>
       </div>
-      <p>${resource.name}</p>
-    </div>
-        `;
-    })
-    .join("");
+          `;
+      })
+      .join("");
+  }
   resourcesList.innerHTML = htmlString;
 };
 
